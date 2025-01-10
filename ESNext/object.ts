cd6714a26b369ca36a96d65905e5eaf4ef3a57dd
hadesyi <leesk55@gmail.com>
2025-01-10 09:25:59 +0900

    ESNext/object.ts 파일 수정: 동적 속성 이름 생성 및 출력 추가

--- ESNext/object.ts
+++ ESNext/object.ts
@@ -5,3 +5,22 @@ const book = {
 
 
 console.log(book.name, book.publisher);
+
+console.log({
+    [1 * 2 * 3]: "hello world",
+});
+
+
+console.log({
+    ["hello" + "world"]: "hello world",
+});
+
+
+function generateProperty(str) {
+    return str + "Key";
+}
+
+
+console.log({
+    [generateProperty("typescript")]: "hello world",
+});
