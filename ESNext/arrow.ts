a467832fde358fd30beabf10323d38e871f56e54
hadesyi <leesk55@gmail.com>
2025-01-10 11:52:11 +0900

    ESNext/arrow.ts 파일 수정: add, plus, sum 함수에 타입 주석 추가

--- ESNext/arrow.ts
+++ ESNext/arrow.ts
@@ -1,16 +1,15 @@
-// 기존에 선언을 하던 방식
-function add(a, b) {
+function add(a: number, b: number): number {
     return a + b;
 }
 
 
 // 화살표 함수(람다 함수)
-const plus = (a, b) => {
+const plus = (a: number, b: number): number => {
     return a + b;
 };
 
 
-const sum = (a, b) => a + b;
+const sum = (a: number, b: number): number => a + b;
 
 
 console.log(add(1, 2), plus(1, 2), sum(1, 2));
