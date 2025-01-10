0b62e4ca121dbed111ba7db0ef04bd3c09318020
hadesyi <leesk55@gmail.com>
2025-01-10 09:24:12 +0900

    literals.ts 파일 수정: null 값 처리 및 여러 줄 문자열 출력 추가

--- ESNext/literals.ts
+++ ESNext/literals.ts
@@ -4,3 +4,13 @@ const hobby = "축구";
 
 console.log("제 이름은 " + author + "입니다. " + hobby + "가 취미입니다.");
 console.log("'는 작은따옴표 \"는 큰따옴표");
+
+const value = null;
+
+
+console.log(`value: ${!!value ? value : "아무 값도 없음"}`);
+console.log(`
+여러줄
+문자열
+`);
+console.log(`3 * 4 = ${3 * 4}`);
