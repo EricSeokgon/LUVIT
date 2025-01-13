4cb5d0d7e3c51178ae96628a0bb2b574e4e3de4d
hadesyi <leesk55@gmail.com>
2025-01-13 11:11:36 +0900

    Decorator/deco.ts 파일 추가: Person 클래스 및 greet 메서드 구현, hello 데코레이터 정의

---
+++ Decorator/deco.ts
@@ -0,0 +1,20 @@
+class Person {
+    name: string;
+
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+
+    @hello
+    greet() {
+        console.log(`hello ${this.name}`);
+    }
+}
+
+
+function hello(originalMethod: any, _context: ClassMethodDecoratorContext) {
+    console.log(originalMethod);
+    console.log(_context);
+}
